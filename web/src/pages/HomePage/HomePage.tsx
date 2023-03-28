a6f0d2ba6021bc86975ef4e2d9028ba078a97561
Ivan <ivanca2003@gmail.com>
2023-03-28 13:10:28 +0200

    homePage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,25 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Link } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-      <body className="h-screen bg-amber-500">
-        <img src="/imgs/logo-vertical.png" alt="" />
-      </body>
+
+      <div className=" flex h-screen flex-col items-center justify-center bg-amber-500">
+        <Link to="#" className="absolute top-10 right-20">
+          Log in
+        </Link>
+        <Link to="#" className="absolute right-40 top-10">
+          Inicio
+        </Link>
+        <img src="/imgs/logo-vertical.png" alt="" className="max-w-min" />
+        <p className="absolute bottom-0 mb-10">
+          App desarrollada por Dalii
+          <br />
+          Solutions & Estudio YOBO
+        </p>
+      </div>
     </>
   )
 }
