ebfb751950ef93c3a6fdc629a61d78bb87645f6a
Ivan <ivanca2003@gmail.com>
2023-04-03 15:05:29 +0200

    feat: :sparkles: Copia de seguridad

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,14 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import MyStack from './src/navigation/Root'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { StyleSheet } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler'
+
+import MyStack from './src/navigation/Root'
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{flex:1}}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
         <NavigationContainer>
           <MyStack></MyStack>
