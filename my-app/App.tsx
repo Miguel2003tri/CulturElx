2e18e3052d183e8ddd716f20e87235b79ce0dc12
Miguel2003tri <llobellmiguel03@gmail.com>
2023-04-03 12:48:09 +0200

    feat(app): :bug: Reparacion de fallo del merge

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react'
-
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-
-const Tab = createMaterialTopTabNavigator()
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
-  )
-}
+import MyStack from './src/navigation/Root'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Ivan Cabrera</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{flex:1}}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <MyStack></MyStack>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   )
 }
 
