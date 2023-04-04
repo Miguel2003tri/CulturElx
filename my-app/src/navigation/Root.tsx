bb6bf1ecd45695ac404e04b3c9e0106f26ccba15
Miguel2003tri <llobellmiguel03@gmail.com>
2023-04-04 13:45:18 +0200

    feat(app): :memo: instalacion de react-native-maps

--- my-app/src/navigation/Root.tsx
+++ my-app/src/navigation/Root.tsx
@@ -5,6 +5,7 @@ import Espacio from '../screens/Espacio'
 import Espacios from '../screens/Espacios'
 import Evento from '../screens/Evento'
 import Eventos from '../screens/Eventos'
+import { StyleSheet } from 'react-native'
 
 import { RootStackParamList, RootTabsParamList } from './types'
 
@@ -13,14 +14,15 @@ const Tabs = createMaterialTopTabNavigator<RootTabsParamList>()
 
 function MyStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator >
       <Stack.Screen
         name="Home"
         component={MyTabs}
-        options={{ title: 'CulturElx', headerTitleAlign: 'center' }}
+        options={{ title: 'CulturElx', headerTitleAlign: 'center'}}
+
       />
-      <Stack.Screen name="Espacio" component={Espacio} />
-      <Stack.Screen name="Evento" component={Evento} />
+      <Stack.Screen name="Espacio" component={Espacio}/>
+      <Stack.Screen name="Evento" component={Evento}/>
     </Stack.Navigator>
   )
 }
@@ -40,5 +42,15 @@ function MyTabs() {
     </Tabs.Navigator>
   )
 }
+const styles = StyleSheet.create({
+  color: {
+    backgroundColor: '#F9BB23',
+
+  },
+  estilo:{
+    backgroundColor: '#F9BB23',
+
+  }
+})
 
 export default MyStack
